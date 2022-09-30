ca27b4bbdf94cf8d899024a1bbe62bcd70ec0df5
Olha Stakhova <olha.stakhova@gmail.com>
2022-09-30 12:59:37 +0300

    added describe app in readme

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 ### Weather App
-### use:
 
-### Demo:
+### In this prodject I use:
+- React;
+- Redux
+- Type script;
+- Bulma from style;
+- Library Echarts;
+- Library Pnotify;
+- API "Foreca Weather";
+
+### About
+
+   In this app we have one pages. 
+   After entering the city, press "Enter" or press the "Search" button
+   we can see if the city name is entered correctly,
+   weather today, for 8 days on the charts the average temperature
+   and in the table more information about it 8 days
+
+   
+   - [DEMO LINK](https://olgastakhova.github.io/weather-app)
--- src/components/WeatherList.tsx
+++ src/components/WeatherList.tsx
@@ -3,41 +3,41 @@ import { useSelector } from "react-redux"
 import { getWeatherList } from "../store/store"
 
 export const WeatherList: FC = () => {
-    const weatherListData = useSelector(getWeatherList);
-    
-    return (
-        <>
-        <h2 className="title is-4"> Weather list</h2>
-        { weatherListData?.length 
-        && <table className="table">
-  <thead>
-    <tr>
-      <th>Data</th>
-      <th>Info</th>
-      <th><abbr title="Max temp ºC">Max</abbr></th>
-      <th><abbr title="Min temp ºC">Min</abbr></th>
-      <th><abbr title="Max wind speed m/s">Wind</abbr></th>
-      <th><abbr title="Cloudiness %">Cloud</abbr></th>
-      <th>Sunrise</th>
-      <th>Sunset</th>
-    </tr>
-  </thead>
+  const weatherListData = useSelector(getWeatherList);
 
-  <tbody>
- { weatherListData.map(item => (
-    <tr key={item.date}>
-      <td>{item.date}</td>
-      <td>{item.symbolPhrase}</td>
-      <td>{item.maxTemp}</td>
-      <td>{item.minTemp}</td>
-      <td>{item.maxWindSpeed}</td>
-      <td>{item.cloudiness}</td>
-      <td>{item.sunrise}</td>
-      <td>{item.sunset}</td>
-    </tr>)
- )}
-  </tbody>
-</table>}
-        </>
-    )
+  return (
+    <>
+      <h2 className="title is-4"> Weather list</h2>
+      {weatherListData?.length
+        && <table className="table">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Info</th>
+              <th><abbr title="Max temp ºC">Max</abbr></th>
+              <th><abbr title="Min temp ºC">Min</abbr></th>
+              <th><abbr title="Max wind speed m/s">Wind</abbr></th>
+              <th><abbr title="Cloudiness %">Cloud</abbr></th>
+              <th>Sunrise</th>
+              <th>Sunset</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {weatherListData.map(item => (
+              <tr key={item.date}>
+                <td>{item.date}</td>
+                <td>{item.symbolPhrase}</td>
+                <td>{item.maxTemp}</td>
+                <td>{item.minTemp}</td>
+                <td>{item.maxWindSpeed}</td>
+                <td>{item.cloudiness}</td>
+                <td>{item.sunrise}</td>
+                <td>{item.sunset}</td>
+              </tr>)
+            )}
+          </tbody>
+        </table>}
+    </>
+  )
 }
